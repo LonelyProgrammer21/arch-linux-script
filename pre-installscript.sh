cd5c0d86c50f4a4323b0e3ebc8e9c55b4f45b1b4
LonelyProgrammer <jeric_tayson@protonmail.com>
2021-12-28 14:13:03 +0800

    Partition Index fix

--- pre-installscript.sh
+++ pre-installscript.sh
@@ -21,7 +21,7 @@ if [[ -e "$SELECTEDSTORAGE" ]]; then
 
     sgdisk -o $SELECTEDSTORAGE
     while [[ $repeat == "true" ]]; do
-        echo "Enter Partition name:"
+        echo -n "Enter Partition name:"
         read PARTITIONLABEL
 
         echo -e "Enter Partition Size (Enter nothing to consumes all the remaining memory)\n Ex. +1GB, +20GB, +512MB:"
@@ -29,8 +29,8 @@ if [[ -e "$SELECTEDSTORAGE" ]]; then
 
         echo -e "Partitioning Partition number:$PARTCOUNT\n"
         sgdisk -n "${PARTCOUNT-1}"::$PARTITIONSIZE -t "${PARTCOUNT-1}":${PARTCODE[$MAXINDEX]} -c "${PARTCOUNT-1}":$PARTITIONLABEL $SELECTEDSTORAGE
-        if [[ $PARTCOUNT -ne 3 ]]; then
         PARTCOUNT=$(( $PARTCOUNT + 1 ))
+        if [[ $PARTCOUNT -lt 4 ]]; then
         MAXINDEX=$PARTCOUNT-1
         fi
         if [[ "${#PARTITIONSIZE}" -ne 0 ]]; then
@@ -86,6 +86,7 @@ if [[ -e "$SELECTEDSTORAGE" ]]; then
              echo "Initializing encryption..."
              sleep 2
              cryptsetup luksFormat ${SELECTEDSTORAGE}4
+             echo "Wait for the prompt and enter your password..."
              cryptsetup open ${SELECTEDSTORAGE}4 crypthome
                 while [[ ! -e "/dev/mapper/crypthome" ]]; do
                     echo "Invalid password, try again."
@@ -104,11 +105,13 @@ if [[ -e "$SELECTEDSTORAGE" ]]; then
     done
 
 mount ${SELECTEDSTORAGE}3 /mnt
+mkdir -p /mnt/boot
+mount ${SELECTEDSTORAGE}1 /mnt/boot
+mkdir -p /mnt/home
+mount ${SELECTEDSTORAGE}4 /mnt/boot
 pacstrap /mnt base base-devel linux-zen linux-zen-headers vim amd-ucode
 
 genfstab -U /mnt >> /mnt/etc/fstab
 else 
     echo -e "Block device is not found Enter a correct device block to continue"
-
-
 fi
