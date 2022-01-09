d596a715bc8fffa6a61fab74906d399ac4a4d31e
LonelyProgrammer <jeric_tayson@protonmail.com>
2022-01-09 15:12:56 +0800

    Add user prompts, variable casing change

--- postinstallscript.sh
+++ postinstallscript.sh
@@ -6,15 +6,23 @@
 # own test case and post-install arch linux install. But you can use it if you want :>
 
 # Variable used for all sorts of statement occuring while execution
-timezone="/usr/share/zoneinfo/Asia/Manila"
-localtime="/etc/localtime"
-grub_target="/boot/"
-username="laynux"
-hostname="laynux-pc"
-USER="xelectro"
+TIMEZONE="/usr/share/zoneinfo/Asia/Manila"
+LOCALTIME="/etc/LOCALTIME"
+GRUB_TARGET="/boot/"
+HOSTNAME=""
+USER=""
 
-# Settings timezone symlinks
-ln -sf $timezone $localtime
+if [[ ! -e "/usr/lib/grub" ]]; then
+	pacman -S --noconfirm grub efibootmgr os-prober 
+
+if [[ -e "/dev/mapper/crypthome" ]]; then
+	PARENTDEV=$(dmsetup deps -o devname /dev/mapper/crypthome | grep -o "(.*)$" | sed -e 's/(//g' -e 's/)//g')
+	HOMEUID=$(blkid -s UUID -o value /dev/$PARENTDEV)
+
+	echo -e "crypthome\t UUID=$HOMEUID\t none\n luks,timeout=150" >> /etc/crypttab
+fi
+# Settings TIMEZONE symlinks
+ln -sf $TIMEZONE $LOCALTIME
 
 # Update rtc clock
 hwclock --systohc
@@ -25,28 +33,55 @@ sed -i 's/#en_US.UTF-8/en_US.UTF-8/' /etc/locale.gen
 # Create a new file for system wide locale setting
 echo LANG=en_US.UTF-8 > /etc/locale.conf
 
-# Create a hostname file for the system
-echo $hostname > /etc/hostname
+while [[ -z $HOSTNAME ]]; do
+
+	read -p "Enter hostname for your pc:" HOSTNAME
+
+	if [[ $HOSTNAME ~= "^*[!\@\#\$%\^&\*\(\)-+_':\\\.]*$" ]]; then
+		HOSTNAME = ""
+	fi
+done
+
+echo "Creating hostname..."
+# Create a HOSTNAME file for the system
+echo $HOSTNAME > /etc/HOSTNAME
 
 # Used to concatenate the existing hostfile, adding the hostname to the existing host file
 echo -e "127.0.0.1\t $hostname\n
 	::1\t $hostname\n127.0.0.1\t${hostname}.localdomain\t $hostname"
 	>> /etc/hosts
 
-# Install the bootloader assuming the EFI partition is mounted to $grub_target
-grub-install --target=x86_64-efi --efi-directory=$grub_target --bootloader-id=ArchLinux
+echo "Installing grub bootloader now..."
+# Install the bootloader assuming the EFI partition is mounted to $GRUB_TARGET
+grub-install --target=x86_64-efi --efi-directory=$GRUB_TARGET --bootloader-id=ArchLinux
 
 # Generating image for grub bootloader next boot
 grub-mkconfig -o /boot/grub/grub.cfg
 
+mkinitcpio -P
+
+while [[ -z $USER ]]; do
+	read -p "Enter your username:" USER
+
+	if [[ $USER ~= "^*[!\@\#\$%\^&\*\(\)-+_':\\\.]*^" ]]; then
+		USER = ""
+		echo -e "Username should not contain any special characters...\nTry Again.\n"
+	fi
+done
+
+# Uncommenting the wheel group to give wheel group superuser permissions
+sed -i 's/^#\s*\(%wheel\s\+ALL=(ALL)\s\+NOPASSWD:\s\+ALL\)/\1/' /etc/sudoers
+
+echo "Creating username..."
 # Creating a normal user and add to the wheel group for sudo permissions
 useradd -mG wheel $USER
 
+echo "Enter password for $USER"
 # Password from for the $USER
 passwd $USER
 
-# Uncommenting the wheel group to give wheel group superuser permissions
-sed -i 's/# wheel/wheel/' /etc/sudoers
+echo "Enter password for root account:"
+passwd
 
 # Install network manager utility, bluetooth. and audio. Last is neofetch ;>
 pacman -S networkmanager bluez pulseaudio pulseaudio-bluetooth neofetch
@@ -58,4 +93,5 @@ systemctl enable bluetooth
 # Flexing i use btw linux user uwu
 neofetch
 echo "All done! :>"
-read -p "Press any key to exit." 
+read -p "Press enter to exit." TMP
+exit
